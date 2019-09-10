8baa1ac63fbbac072dd3845a8dea8b1b3576625a
Kevin Louis <kevin.santos@ateliware.com.br>
2019-09-10 17:48:00 -0300

    improve middleware

--- auth/app/middlewares.js
+++ auth/app/middlewares.js
@@ -1,18 +1,25 @@
 const TokenService = require('./services/tokenService');
+const UserModel = require('./models/userModel');
 
 const auth = async function (req, res, next) {
-    const token = req.get('authorization')
+    const token = req.get('authorization');
 
     if (!token) {
         return res.status(403).json({error: 'token not provided'})
     }
 
-    const tokenCleared = token.replace('Bearer ', '')
+    const tokenCleared = token.replace('Bearer ', '');
 
     const isBlackListed = await TokenService.isBlackListed(tokenCleared);
 
     if(isBlackListed){
         return res.status(401).json({error: 'Sessão Expirada'})
+    }
+
+    const user = await UserModel.findByToken(tokenCleared);
+
+    if(!user){
+        return res.status(404).json({error: 'Usuário não encontrado'})
     }
 
     const isExpired = TokenService.isExpired(tokenCleared);
