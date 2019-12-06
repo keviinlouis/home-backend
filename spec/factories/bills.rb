f7d7017f16844b358b17039043f9038f43813199
Kevin Louis <kevin.santos@ateliware.com.br>
2019-12-06 14:41:39 -0300

    fix bills factory

--- spec/factories/bills.rb
+++ spec/factories/bills.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     amount { Faker::Number.decimal(r_digits: 3) }
     name { Faker::Superhero.unique.name }
     description { Faker::Cannabis.brand }
-    expires_at { Faker::Date.between(from: Date.today, to: Date.today + 5.years) }
+    expires_at { Faker::Date.between(from: Date.tomorrow, to: Date.tomorrow + 5.years) }
     frequency { rand(30) }
     frequency_type { Bill.frequency_types.keys.sample }
     user
